9e0fd267d54f087e99021f3fd18c0cb39a8b9f87
shibin <shibiny6@gmail.com>
2021-11-10 23:06:57 +0800

    feat: 🎸 add decorator

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -1,22 +1,43 @@
-/**
- * 
- * @param targetClassPrototype 
- * @param key 
- * @param methodDecri 
- */
-function myDecorator(targetClassPrototype: any, key: string,methodDecri: PropertyDecorator){
-    console.log(targetClassPrototype)
-    console.log(key)
-    console.log(methodDecri)
-}
-class Person{
-    public rolename: string = '管理员'
-    constructor(){
+// /**
+//  * 
+//  * @param targetClassPrototype 
+//  * @param key 
+//  * @param methodDecri 
+//  */
+// function myDecorator(targetClassPrototype: any, key: string,methodDecri: PropertyDecorator){
+//     console.log(targetClassPrototype)
+//     console.log(key)
+//     console.log(methodDecri)
+// }
+// class Person{
+//     public rolename: string = '管理员'
+//     constructor(){
 
-    }
-    @myDecorator
-    DistriRoles(){
-        console.log('分配角色')
+//     }
+//     @myDecorator
+//     DistriRoles(){
+//         console.log('分配角色')
+//     }
+// }
+// new Person()
+function logger(targetClassPrototype: any) {
+    // const a = new targetClassPrototype()
+    // a.say()
+    console.log('log')
+}
+function route(path: string){
+    return (targetClassPrototype: any) => {
+        // const a = new targetClassPrototype()
+        // a.say()
+        console.log('route',path)
     }
 }
-new Person()
+@logger
+@route('/login')
+class Person {
+    name: string = 'shibin'
+    say(){
+        console.log(this.name)
+    }
+}
+// new Person()
